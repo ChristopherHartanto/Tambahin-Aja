db83047a11a425544f136df494336fe36806c4b8
ChristopherHartanto <christopherhartanto999gmail.com>
2020-04-26 00:50:37 +0700

    - Adding Waiting Presenter & View

---
+++ app/src/main/java/com/example/balapplat/presenter/WaitingPresenter.kt
@@ -0,0 +1,193 @@
+package com.example.balapplat.presenter
+
+import android.util.Log
+import com.example.balapplat.play.CountdownActivity
+import com.example.balapplat.play.OpponentOnline
+import com.example.balapplat.play.Status
+import com.example.balapplat.view.MainView
+import com.example.balapplat.view.WaitingView
+import com.facebook.Profile
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.toast
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.HashMap
+
+class WaitingPresenter(private val view: WaitingView, private val database: DatabaseReference) {
+
+    fun getWaitingList(){
+        val postListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists())
+                {
+                    for (data in p0.children){
+                        if (data.child("status").value == false && !data.key.equals(Profile.getCurrentProfile().id)){
+                            view.loadData(data, false)
+
+                            val values: HashMap<String, Any> = hashMapOf(
+                                "name" to Profile.getCurrentProfile().name,
+                                "facebookId" to Profile.getCurrentProfile().id,
+                                "status" to true
+                            )
+                            database.child("waitingList").child(data.key!!).setValue(values).addOnFailureListener {
+                                it.message?.let { it1 -> view.response(it1) }
+                            }
+                            return
+                        }
+                        if (data.key.equals(Profile.getCurrentProfile().id)){
+                            getResponseOnline()
+                            return
+                        }
+
+                    }
+
+                }
+                else
+                    registerToWaitingList()
+
+            }
+
+        }
+        database.child("waitingList").addValueEventListener(postListener)
+
+    }
+
+    fun registerToWaitingList(){
+        val values: HashMap<String, Any> = hashMapOf(
+            "name" to Profile.getCurrentProfile().name,
+            "facebookId" to Profile.getCurrentProfile().id,
+            "status" to false
+        )
+        database.child("waitingList").child(Profile.getCurrentProfile().id).setValue(values).addOnSuccessListener {
+            getResponseOnline()
+        }.addOnFailureListener {
+            it.message?.let { it1 -> view.response(it1) }
+        }
+    }
+
+    private fun getResponseOnline(){
+
+        val postListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists())
+                {
+                    if (p0.getValue(OpponentOnline::class.java)!!.status == true){
+                        view.loadData(p0, true)
+                        createGame(Profile.getCurrentProfile().id,true)
+                        removeWaitingList()
+                    }
+
+                }
+            }
+
+        }
+        database.child("waitingList").child(Profile.getCurrentProfile().id).addValueEventListener(postListener)
+    }
+
+    fun makeInvitation(facebookId : String){
+
+        val values: HashMap<String, Any?> = hashMapOf(
+            "name" to Profile.getCurrentProfile().name,
+            "facebookId" to Profile.getCurrentProfile().id,
+            "status" to false
+        )
+
+        database.child("invitation").child(facebookId).setValue(values).addOnSuccessListener {
+            view.response("invitationSent")
+
+
+        }.addOnFailureListener {
+            it.message?.let { it1 -> view.response(it1) }
+        }
+    }
+
+    fun getResponse(facebookId : String){
+        val postListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists())
+                {
+                    Log.i("cek1",""+p0)
+                    if(p0.getValue(Status::class.java)!!.status!!){
+                        view.response("accepted")
+
+                    }
+
+                }
+                //else{
+
+//                           alert{
+//                            title = "Reject"
+//                            yesButton {
+//                                startActivity(intentFor<MainActivity>())
+//                            }
+//                               noButton {  }
+//                        }.show()
+      //          }
+            }
+
+        }
+        database.child("invitation").child(facebookId).addValueEventListener(postListener)
+    }
+
+    fun createGame(facebookId: String, playOnline: Boolean){
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+
+        val values: HashMap<String, Any> = hashMapOf(
+            "player1" to 0,
+            "player2" to 0,
+            "pause" to false
+        )
+        database.child("onPlay").child(facebookId).setValue(values).addOnSuccessListener {
+            if (!playOnline)
+            view.response("createGame")
+        }.addOnFailureListener {
+            it.message?.let { it1 -> view.response(it1) }
+        }
+    }
+
+    fun removeInvitation(facebookId: String){
+        database.child("invitation").child(facebookId).removeValue()
+    }
+
+    fun removeWaitingList(){
+        database.child("waitingList").child(Profile.getCurrentProfile().id).removeValue()
+    }
+
+    fun loadTips(){
+        val postListener = object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if(p0.exists())
+                {
+                    val count = p0.childrenCount - 1
+                    val rand = Random().nextInt(count.toInt()) + 1
+
+                    view.loadTips(p0.child(rand.toString()).value as String)
+                }
+
+            }
+
+        }
+        database.child("tips").addValueEventListener(postListener)
+    }
+}
---
+++ app/src/main/java/com/example/balapplat/view/WaitingView.kt
@@ -0,0 +1,9 @@
+package com.example.balapplat.view
+
+import com.google.firebase.database.DataSnapshot
+
+interface WaitingView {
+    fun loadData(dataSnapshot: DataSnapshot, creator: Boolean)
+    fun response(message: String)
+    fun loadTips(tips: String)
+}
