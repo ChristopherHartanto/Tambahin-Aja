20d6cbeba3a834671acedee90bc593ed3900dd20
ChristopherHartanto <christopherhartanto999gmail.com>
2020-04-26 16:24:22 +0700

    fetch data from firebase

--- app/src/main/java/com/example/balapplat/TournamentFragment.kt
+++ app/src/main/java/com/example/balapplat/TournamentFragment.kt
@@ -1,21 +1,23 @@
 package com.example.balapplat
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.balapplat.leaderboard.LeaderBoardRecyclerViewAdapter
 import com.example.balapplat.model.HighScore
 import com.example.balapplat.model.User
 import com.google.firebase.database.*
-import kotlinx.android.synthetic.main.activity_leader_board.*
 import kotlinx.android.synthetic.main.fragment_tournament.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.jetbrains.anko.support.v4.ctx
-import org.jetbrains.anko.toast
+import java.text.SimpleDateFormat
+import java.time.Duration
+import java.util.*
+
 
 class Tournament : Fragment() {
 
@@ -48,7 +50,7 @@ class Tournament : Fragment() {
                 }
 
             }
-            database.child("highscore").orderByChild("score").addValueEventListener(postListener)
+            database.child("highscore").orderByChild("score").addListenerForSingleValueEvent(postListener)
 
         }
     }
@@ -79,7 +81,7 @@ class Tournament : Fragment() {
                 }
 
             }
-            database.child("users").child(id).addValueEventListener(postListener)
+            database.child("users").child(id).addListenerForSingleValueEvent(postListener)
 
         }
     }
@@ -93,7 +95,8 @@ class Tournament : Fragment() {
 
         items.clear()
         profileItems.clear()
-        retrieve()
+
+        fetchTournament()
         super.onStart()
     }
 
@@ -104,4 +107,61 @@ class Tournament : Fragment() {
         profileItems.add(item)
         adapter.notifyDataSetChanged()
     }
+
+    fun fetchTournament(){
+        GlobalScope.launch {
+            val postListener = object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    if (p0.exists()){
+                        loadData(p0,true)
+                    }else
+                        loadData(p0,false)
+                }
+
+            }
+            database.child("tournament").addListenerForSingleValueEvent(postListener)
+
+        }
+    }
+
+    fun loadData(dataSnapshot: DataSnapshot, status: Boolean){
+        val data = dataSnapshot.getValue(TournamentData::class.java)
+
+        if (data != null && status) {
+            tvTournamentTitle.text = data.title
+            tvTournamentDesc.text = data.description
+            tvTournamentTimeLeft.text = data.deadLine
+
+            retrieve()
+//            GlobalScope.launch {
+//                val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+//                val currentDate = sdf.format(Date())
+//
+//                Duration.
+//                val diff: Long = Date.from() - date2.getTime()
+//                val seconds = diff / 1000
+//                val minutes = seconds / 60
+//                val hours = minutes / 60
+//                val days = hours / 24
+//            }
+        }
+        else{
+            tvTournamentTitle.text = "No Tournament Right Now"
+            tvTournamentDesc.text = ""
+            tvTournamentTimeLeft.text = ""
+        }
+    }
 }
+
+data class TournamentData(
+    var Reward1: Long? = 0,
+    var Reward2: Long? = 0,
+    var Reward3: Long? = 0,
+    var description: String? = "",
+    var deadLine: String? = "",
+    var title: String? = ""
+)
